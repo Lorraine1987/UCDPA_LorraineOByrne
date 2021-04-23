78c42f3ac8d2569e97ee4a16c184e40f591c71ad
Lorraine1987 <obyrneloraine@hotmail.com>
2021-04-23 01:10:39 +0100

    Continuation of Project

--- Rubric Project.py
+++ Rubric Project.py
@@ -1,5 +1,25 @@
-# Import Pandas
+# Import Modules
 import pandas as pd
+import numpy as np
 
-def importdata(netflix_titles.csv):
-            data=pd.read_csv()
+df = pd.read_csv('healthcare-dataset-stroke-data.csv')
+
+print(df.head())
+
+print(df.info())
+
+print(df.shape)
+
+# Check for missing values
+missing_values = df.isnull()
+print(missing_values)
+
+print(missing_values.sum())
+
+# Replace missing values with average
+df2 = df.fillna(df.mean())
+print(df2.head())
+print(df2.shape)
+
+
+
